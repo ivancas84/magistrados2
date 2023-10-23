c0197b1e7c422637a92a8d3d18aad1ac0ef12d7a
Iván Castañeda <icastaneda@scba.gov.ar>
2023-10-23 13:49:10 -0300

    Incorporar nuevo método CollectionUtils.SetPropertyValue. No estoy seguro si va en esta clase, pero por el momento lo dejo (https://github.com/SqlOrganize/SqlOrganizeCs/issues/15).

--- Utils/CollectionsUtils.cs
+++ Utils/CollectionsUtils.cs
@@ -358,8 +358,22 @@ namespace Utils
             }
             return dictionaryString.TrimEnd(',', ' ') + "}";
         }
-    }
 
+        /// <summary>
+        ///     A T extension method that sets property value.
+        /// </summary>
+        /// <typeparam name="T">Generic type parameter.</typeparam>
+        /// <param name="this">The @this to act on.</param>
+        /// <param name="propertyName">Name of the property.</param>
+        /// <param name="value">The value.</param>
+        /// <remarks>https://csharp-extension.com/en/method/1002738/object-setpropertyvalue</remarks>
+        public static void SetPropertyValue<T>(this T @this, string propertyName, object? value)
+        {
+            Type type = @this.GetType();
+            PropertyInfo property = type.GetProperty(propertyName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+            property.SetValue(@this, value, null);
+        }
+    }
 
 }
 
