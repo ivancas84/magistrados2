fe8a4c7196c8003f2f0578037724a2ffda6975bf
ivancas84 <ivancas84@gmail.com>
2024-02-23 08:35:27 -0300

    Agregar modificador virtual.

--- ModelOrganize/Config.cs
+++ ModelOrganize/Config.cs
@@ -2,52 +2,52 @@
 {
     public class Config
     {
-        public string connectionString { get; set; }
-        public string dbName { get; set; }
+        public virtual string connectionString { get; set; }
+        public virtual string dbName { get; set; }
 
         /// <summary>
         /// Indica el lugar donde se generaran los archivos json para documentar el modelo
         /// </summary>
-        public string docPath { get; set; } = "./Doc/";
+        public virtual string docPath { get; set; } = "./Doc/";
 
         /// <summary>
         /// Alias reservados que no deben ser incluidos en el modelo
         /// </summary>
-        public List<string> reservedAlias { get; set; } = new List<string>();
+        public virtual List<string> reservedAlias { get; set; } = new List<string>();
 
         /// <summary>
         /// Entidades reservadas que no seran incluidas en el modelo
         /// </summary>
-        public List<string> reservedEntities { get; set; } = new List<string>();
+        public virtual List<string> reservedEntities { get; set; } = new List<string>();
 
 
         /// <summary>
         /// Indica el lugar donde se tomara la configuracion para generar el modelo
         /// </summary>
-        public string configPath { get; set; } = "./Config";
+        public virtual string configPath { get; set; } = "./Config";
 
         /// <summary>
         /// Indica el lugar donde se almacenaran las clases de datos
         /// </summary>
-        public string dataClassesPath { get; set; } = "./App/Data";
+        public virtual string dataClassesPath { get; set; } = "./App/Data";
 
         /// <summary>
         /// Indica el namespace utilizado para las clases de datos
         /// </summary>
-        public string dataClassesNamespace { get; set; } = "App.Data";
+        public virtual string dataClassesNamespace { get; set; } = "App.Data";
 
         /// <summary>
         /// Indica el lugar donde se almacenara la clase que describe el esquema de la base de datos
         /// </summary>
-        public string schemaClassPath { get; set; } = "./";
+        public virtual string schemaClassPath { get; set; } = "./";
 
         /// Indica el namespace asignado a la clase Schema
-        public string schemaClassNamespace { get; set; } = "App";
+        public virtual string schemaClassNamespace { get; set; } = "App";
 
         /// <summary>
         /// Referencia para definir los alias e identificadores de fields 
         /// </summary>
-        public string idSource { get; set; } = "entity_name"; //field_name or entity_name
+        public virtual string idSource { get; set; } = "entity_name"; //field_name or entity_name
 
         /// <summary>
         /// Nombre del identificador único de las tablas       
@@ -56,7 +56,7 @@
         /// Todas las tablas deben tener un identificador, que puede ser real o ficticio<br/>
         /// El identificador ficticio se define como "_Id"
         /// </remarks>
-        public string id = "_Id";
+        public virtual string id { get; set; } = "_Id";
 
     }
 }
